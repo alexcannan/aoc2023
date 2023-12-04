2ce489f5f66d1a96d1f7b978cc415a1bff008386
Alex Cannan <alexfcannan@gmail.com>
2023-12-04 09:47:51 -0500

    day4part2

--- aor/src/aoc4.rs
+++ aor/src/aoc4.rs
@@ -1,10 +1,46 @@
+use std::collections::HashMap;
+
 use crate::download;
+
+
+struct Stack {
+    counts: Vec<i32>,
+}
+
+impl Stack {
+    fn new() -> Stack {
+        Stack { counts: Vec::new() }
+    }
+
+    // add 1 to the first n items of stack, create if they don't exist
+    fn add(&mut self, n: usize) {
+        for i in 0..n {
+            if i < self.counts.len() {
+                self.counts[i] += 1;
+            } else {
+                self.counts.push(1);
+            }
+        }
+    }
+
+    // pop first item from stack off
+    fn pop(&mut self) -> i32 {
+        if self.counts.len() == 0 {
+            return 0;
+        }
+        let count = self.counts[0];
+        self.counts.remove(0);
+        return count;
+    }
+}
 
 
 pub fn main() {
     let input = download::get_input("https://adventofcode.com/2023/day/4/input").expect("Failed to download input");
     let mut score: i32 = 0;
     let mut card_score: i32;
+    let mut count_stack: Stack = Stack::new();
+    let mut n_cards: i32 = 0;
     for line in input.lines() {
         println!("{}", line);
         // take everything after colon
@@ -19,13 +55,21 @@ pub fn main() {
                 n_wins += 1;
             }
         }
+        let extra_cards: i32 = count_stack.pop();
         if n_wins == 0 {
             card_score = 0;
         } else {
             card_score = 2_i32.pow((n_wins-1) as u32);
+            for _ in 0..extra_cards+1 {
+                count_stack.add(n_wins as usize);
+            }
         }
         println!("{} wins, score {}", n_wins, card_score);
+        // print count stack
+        println!("{} extra; count stack: {:?}", extra_cards, count_stack.counts);
         score += card_score;
+        n_cards += 1 + extra_cards;
     }
     println!("Score: {}", score);
+    println!("Cards: {}", n_cards);
 }
